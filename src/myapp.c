91acbbafb98298c0e39a1c5793c7426be70debf7
Seonjin Jeong <tjswls551@gmail.com>
2022-11-03 17:54:41 +0900

    some comments are added

--- src/myapp.c
+++ src/myapp.c
@@ -1,10 +1,13 @@
 #include "myfuncs.h"
 
 int main(){
-for(int i=0; i<3; i++){
+	//call functions 3 times...
+	for(int i=0; i<3; i++){
 	foo();
-}
+	}
+	
 	goo();
+	
 	for(int i=0; i<3; i++){
 	zoo();
 	}
